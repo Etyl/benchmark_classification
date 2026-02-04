0df2d22acc4c4229b3e3879c29ea7c02c9dc4c3e
Hippolyte Verninas <etyl.sani@gmail.com>
2026-02-04 17:00:28 +0100

    doc: add comments

--- plots/solver_params.py
+++ plots/solver_params.py
@@ -3,19 +3,21 @@ from benchopt import BasePlot
 
 class Plot(BasePlot):
     name = "Solver Params"
-    type = "bar_chart"
+    type = "bar_chart" # Or "scatter", "boxplot", "table"
     options = {
-        "dataset": ...,
-        "solver": ["SVM", "torch-logreg"],
+        "dataset": ..., # Automatic options from DataFrame columns
+        "solver": ["SVM", "torch-logreg"], # custom options
         "objective": ...,
         "objective_column": ...,
     }
 
+    # The inputs args of this method correspond to `df` and
+    # the keys in the `options` dictionary.
     def plot(self, df, dataset, solver, objective, objective_column):
+        # Filter the DataFrame
         df = df.query(
             "dataset_name == @dataset and objective_name == @objective"
         )
-
         plots = []
         for solver_name, df_filtered in df.groupby('solver_name'):
             if solver not in solver_name:
@@ -25,10 +27,15 @@ class Plot(BasePlot):
             if objective_column not in df_filtered:
                 continue
 
+            # For each solver, get the last recorded value of the
+            # objective metric for each run
             stop_val = df_filtered['stop_val'].max()
             this_df = df_filtered[df_filtered['stop_val'] == stop_val]
             y = this_df[objective_column].tolist()
 
+            # For a bar chart, each plot is a dictionary with keys:
+            # 'y', 'text', 'label', 'color'
+            # For other plot types, the structure may differ (see documentation).
             plots.append({
                 "y": y,
                 "text": "",
@@ -38,6 +45,8 @@ class Plot(BasePlot):
 
         return plots
 
+    # The inputs args of this method correspond to `df` and
+    # the keys in the `options` dictionary.
     def get_metadata(self, df, dataset, solver, objective, objective_column):
         return {
             "title": f"{objective}\nData: {dataset}\nSolver: {solver}",
